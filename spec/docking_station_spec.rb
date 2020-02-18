18afd6de916e8b5ceb137b223ec5cd93548b6df1
Student <student@Makerss-MacBook-Air-2.local>
2020-02-18 16:22:39 +0000

    to clear

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,4 +8,19 @@ describe DockingStation do
     bike = docking_station.release_bike
     expect(bike).to be_working
   end
+  it 'dock something' do
+    bike = docking_station.dock(bike)
+    expect(bike).respond_to?(:bike)
+  end
+  it 'dock bikes' do
+    bike = Bike.new
+    expect(docking_station.dock(bike)).to eq bike
+  end
+
+  it 'dock bike again' do
+    bike = Bike.new
+    docking_station.dock(bike)
+    expect(docking_station.dock(bike)).to eq bike
+  
+  end
 end
